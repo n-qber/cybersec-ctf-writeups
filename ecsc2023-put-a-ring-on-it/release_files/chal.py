1098ac64e7a9b8ed3a0b422c9e61068283d40890
Nicole Ramos <ncr170105@gmail.com>
2026-09-01 11:56:14 -0300

    feat: solutions and comments about the logic

--- ecsc2023-put-a-ring-on-it/release_files/chal.py
+++ ecsc2023-put-a-ring-on-it/release_files/chal.py
@@ -10,6 +10,7 @@ from Crypto.Cipher import AES
 import ed25519
 
 NUM_LEVELS = 16
+
 RING_SIZE = 16
 
 rand = random.SystemRandom()
@@ -59,7 +60,7 @@ def hash_to_point(val):
 def key_image(privkey):
     return scalarmultKeyInt(hash_to_point(public_key(privkey)), privkey)
 
-
+# Função mais importante para resolver
 def ring_sign(message, public_keys, my_privkey, my_pubkey, my_index):
     image = key_image(my_privkey)
 
@@ -72,10 +73,12 @@ def ring_sign(message, public_keys, my_privkey, my_pubkey, my_index):
     for i in range(RING_SIZE):
         if i == my_index:
             q = random_scalar()
+
             L = public_key(q)
             R = scalarmultKeyInt(hash_to_point(my_pubkey), q)
         else:
             q = random_scalar()
+
             w = random_scalar()
 
             L = addKeys(public_key(q), scalarmultKeyInt(public_keys[i], w))
@@ -85,6 +88,8 @@ def ring_sign(message, public_keys, my_privkey, my_pubkey, my_index):
             )
 
             sigc[i] = w
+
+            # se a chave usada para assinar "my_index" nao for a 15, a última iteração cairá aqui. Porém, isso significa que o valor de sigr[15] será q fora do loop.
             sigr[i] = q
             sumc += sigc[i]
 
@@ -95,10 +100,26 @@ def ring_sign(message, public_keys, my_privkey, my_pubkey, my_index):
     c = scalar_hash(buf)
 
     sigc[my_index] = (c - sumc) % ed25519.l
+
+    # Ao invés de utilizar o gerado quando i == my_index, utiliza-se o valor de q que foi gerado na última iteração do loop, ou seja, utiliza-se sigr[15] (se i == 15 não for my_index). 
     sigr[my_index] = (q - sigc[my_index] * my_privkey) % ed25519.l
 
     return image, sigc, sigr
+# após esse loop, o valor de q ainda se mantém, e é usado para calcular sigr[my_index] no final do loop. Isso é intencional, pois q é o valor aleatório gerado para a chave privada do usuário na posição my_index, e é necessário para calcular a assinatura correta.
 
+# sendo rig_size = 16, a última iteração será i = 15.
+
+'''
+ Como usar o oráculo:
+
+ 1. Para descobrir o my_index, itera-se pelas 15 possibilidades utilizando a fórmula:
+
+ my_privkey = (q - sigr[i]) * pow(sigc[i], -1, ed25519.l) % ed25519.l
+
+ 2. Com isso, gera-se uma chave "falsa" my_privkey para cada possibilidade. E, passando essa chave pela função key_image, obtém-se a chave pública correspondente. Se a chave pública gerada for igual à chave pública do nível, então o my_index correto foi encontrado.
+
+ 3. 
+'''
 
 def gen_ring(msg):
     public_keys = [public_key(random_scalar()) for i in range(RING_SIZE)]
